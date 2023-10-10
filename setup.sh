7ecb74331f60282f44bf40fe4fdee47eecc459e1
Ricardo Oliveira <ricardo@hashicorp.com>
2023-10-10 09:30:28 +0100

    removing escapes

--- setup.sh
+++ setup.sh
@@ -16,26 +16,26 @@ echo
 echo "Please enter your HCP Vault admin token."
 read -s VAULT_TOKEN
 echo
-echo "GitHub User ID : \$GITHUB_OWNER"
-# echo "GitHub Personal Access Token : \$GITHUB_TOKEN"
-echo "HCP Vault hostname : \$VAULT_ADDR"
-# echo "HCP Vault token : \$VAULT_TOKEN"
+echo "GitHub User ID : $GITHUB_OWNER"
+# echo "GitHub Personal Access Token : $GITHUB_TOKEN"
+echo "HCP Vault hostname : $VAULT_ADDR"
+# echo "HCP Vault token : $VAULT_TOKEN"
 echo
 read -p "Do these look right to you? Y/n " -n 1 -r
 echo
-if [[ ! \$REPLY =~ ^[Yy]\$ ]]
+if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
   echo "Please try again."
   exit 1
 fi
 echo
 
-export GITHUB_TOKEN=\$GITHUB_TOKEN
-export GITHUB_OWNER=\$GITHUB_OWNER
-export VAULT_ADDR=\$VAULT_ADDR
-export VAULT_TOKEN=\$VAULT_TOKEN
+export GITHUB_TOKEN=$GITHUB_TOKEN
+export GITHUB_OWNER=$GITHUB_OWNER
+export VAULT_ADDR=$VAULT_ADDR
+export VAULT_TOKEN=$VAULT_TOKEN
 
 cat >> terraform.tfvars <<EOM
-github_org="\$GITHUB_OWNER"
-vault_address="\$VAULT_ADDR"
+github_org="$GITHUB_OWNER"
+vault_address="$VAULT_ADDR"
 EOM
